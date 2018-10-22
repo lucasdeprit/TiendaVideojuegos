74df432d2bad5cdd9786ab78c303cf2916a559b8
lucas Deprit <lucas.deprit@opendeusto.es>
2018-10-22 09:18:45 +0200

    //cmbio

--- TiendaVideojuegos/src/logica/GestionTienda.java
+++ TiendaVideojuegos/src/logica/GestionTienda.java
@@ -13,9 +13,9 @@ public class GestionTienda {
 		Cliente c2 = new Cliente ("79127223J","luis",1996,"15/12/96","blabla madrid",63455434,7886,"lustin","777kiiki777Le76");
 		Cliente c3 = new Cliente ("78122334L","MAthew",1995,"07/9/95","blabla barcelona ",695204444,7885,"mateo334","7me llamo76");
 	
-		System.out.println(v1.toString()+v2.toString()+v3.toString());
+		System.out.println(v1.toString()+"\n"+v2.toString()+"\n"+v3.toString());
 		System.out.println(c1.toString()+"\n"+c2.toString()+"\n"+c3.toString());
-		
+		System.out.println("cambio");
 		
 		
 	}
