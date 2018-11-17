cc4f2e922237c2a70c2f28bc04881cfd76d86143
joel <joel@DESKTOP-5LDJ9UU>
2018-11-17 19:23:29 +0100

    Intentar guardar registro base datos

--- TiendaVideojuegos/src/Gui/Vregistrar.java
+++ TiendaVideojuegos/src/Gui/Vregistrar.java
@@ -1,6 +1,7 @@
 package Gui;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
@@ -12,57 +13,90 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 
 import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+
+import java.awt.Event;
+import java.awt.EventQueue;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 public class Vregistrar extends JFrame{
-	
+	private Connection conexion;
 	public static Logger logger = Logger.getLogger(Vregistrar.class.getName()) ;
-	
-	private JPasswordField passwordField;
 	JLabel lblNombre = new JLabel("Nombre:");
 	JLabel lblApellidos = new JLabel("Apellidos:");
-	JTextArea nombretxt = new JTextArea();
-	JTextArea apellidostxt = new JTextArea();
-	JSpinner spinneredad = new JSpinner();
 	JLabel lblEdad = new JLabel("Edad:");
 	JLabel lblCorreo = new JLabel("Direccion correo:");
 	JLabel lblNick = new JLabel("Nick:");
 	JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
-	JTextArea nicktxt = new JTextArea();
-	JTextArea correotxt = new JTextArea();
 	JButton btnAceptar = new JButton("Aceptar");
 	JCheckBox checkCondiciones = new JCheckBox("Acepto que mis datos sean guardados en la base de datos");
-	public void guardarDatosusuario() {
-		//
-		int edad;
-		String texto = nombretxt.getText();
-	    String nombre = "usuarios.txt";
-	    try{
-	      FileWriter fichero = new FileWriter("C:\\Users\\Joel\\git\\TiendaVideojuegos\\TiendaVideojuegos"+nombre);
-	      //Insertamos el texto creado y si trabajamos
-	      //en Windows terminaremos cada línea con "\r\n"
-	      fichero.write("Nombre:"+texto + "\r\n");
-	      texto=apellidostxt.getText();
-	      fichero.write("Apellidos:"+texto + "\r\n");
-	      texto=correotxt.getText();
-	      fichero.write("Correo:"+texto + "\r\n");
-	      texto=nicktxt.getText();
-	      fichero.write("Nick:"+texto + "\r\n");
-	      texto=passwordField.getText();
-	      fichero.write("Pass:"+texto + "\r\n");
-	    // edad= spinneredad.get;
-	      //cerramos el fichero
-	      fichero.close();
+	private JTextField Nombre;
+	private JTextField Apellidos;
+	private JTextField Edad;
+	private JTextField DireccionCorreo;
+	private JTextField Nick;
+	private JTextField Contrasena;
 
-	    }catch(Exception ex){
-	      ex.printStackTrace();
-	    }
+	public void Enviar_datos() {
+		
+		try {
+			final String consulta = "INSERT INTO registros(NOMBRE,APELLIDOS,EDAD,CORREO ELECTRONICO,NICK,CONTRASEÑA) VALUES(?,?,?,?,?,?)";
+			PreparedStatement sentencia = conexion.prepareStatement(consulta);
+			sentencia.setString(1, Nombre.getText());
+			sentencia.setString(2, Apellidos.getText());
+			sentencia.setString(3, Edad.getText());
+			sentencia.setString(4, DireccionCorreo.getText());
+			sentencia.setString(5, Nick.getText());
+			sentencia.setString(6, Contrasena.getText());
+			
+			sentencia.executeUpdate();
+			Nombre.setText("");
+			Apellidos.setText("");
+			Edad.setText("");
+			DireccionCorreo.setText("");
+			Nick.setText("");
+			Contrasena.setText("");
+			
+		}catch(Exception e) {
+			e.getStackTrace();
+			JOptionPane.showInputDialog(null,"Error al conectar a la base de datos");
+		}
 	}
+	
+//	public void guardarDatosusuario() {
+//		//
+//		int edad;
+//		String texto = nombretxt.getText();
+//	    String nombre = "usuarios.txt";
+//	    try{
+//	      FileWriter fichero = new FileWriter("C:\\Users\\Joel\\git\\TiendaVideojuegos\\TiendaVideojuegos"+nombre);
+//	      //Insertamos el texto creado y si trabajamos
+//	      //en Windows terminaremos cada línea con "\r\n"
+//	      fichero.write("Nombre:"+texto + "\r\n");
+//	      texto=apellidostxt.getText();
+//	      fichero.write("Apellidos:"+texto + "\r\n");
+//	      texto=correotxt.getText();
+//	      fichero.write("Correo:"+texto + "\r\n");
+//	      texto=nicktxt.getText();
+//	      fichero.write("Nick:"+texto + "\r\n");
+//	      texto=passwordField.getText();
+//	      fichero.write("Pass:"+texto + "\r\n");
+//	    // edad= spinneredad.get;
+//	      //cerramos el fichero
+//	      fichero.close();
+//
+//	    }catch(Exception ex){
+//	      ex.printStackTrace();
+//	    }
+//	}
 	public Vregistrar() {
 		setResizable(false);
 		setSize(460,296);
@@ -74,32 +108,18 @@ public class Vregistrar extends JFrame{
 		getContentPane().add(lblNombre);
 		lblApellidos.setBounds(32, 56, 61, 22);
 		getContentPane().add(lblApellidos);
-		nombretxt.setBounds(184, 30, 109, 22);
-		getContentPane().add(nombretxt);
-		apellidostxt.setBounds(184, 56, 109, 22);
-		getContentPane().add(apellidostxt);
-		spinneredad.setModel(new SpinnerNumberModel(3, 3, 100, 1));
-		spinneredad.setBounds(214, 178, 30, 22);
-		getContentPane().add(spinneredad);
-		lblEdad.setBounds(32, 178, 43, 22);
+		lblEdad.setBounds(32, 87, 43, 22);
 		getContentPane().add(lblEdad);
-		lblCorreo.setBounds(32, 91, 109, 16);
+		lblCorreo.setBounds(32, 120, 109, 16);
 		getContentPane().add(lblCorreo);
-		lblNick.setBounds(32, 120, 56, 16);
+		lblNick.setBounds(32, 147, 56, 16);
 		getContentPane().add(lblNick);
-		lblContrasea.setBounds(23, 149, 70, 16);
+		lblContrasea.setBounds(32, 174, 70, 16);
 		getContentPane().add(lblContrasea);
-		nicktxt.setBounds(184, 117, 106, 16);
-		getContentPane().add(nicktxt);
-		passwordField = new JPasswordField();
-		passwordField.setBounds(184, 146, 124, 19);
-		getContentPane().add(passwordField);
-		correotxt.setBounds(184, 88, 109, 19);
-		getContentPane().add(correotxt);
 		btnAceptar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent c) {
-				guardarDatosusuario();
+				Enviar_datos();
 				
 			}
 		});
@@ -108,9 +128,44 @@ public class Vregistrar extends JFrame{
 		checkCondiciones.setBounds(94, 215, 373, 25);
 		getContentPane().add(checkCondiciones);
 		
+		Nombre = new JTextField();
+		Nombre.setBounds(184, 29, 109, 22);
+		getContentPane().add(Nombre);
+		Nombre.setColumns(10);
+		
+		Apellidos = new JTextField();
+		Apellidos.setBounds(184, 57, 201, 20);
+		getContentPane().add(Apellidos);
+		Apellidos.setColumns(10);
+		
+		Edad = new JTextField();
+		Edad.setBounds(184, 88, 28, 20);
+		getContentPane().add(Edad);
+		Edad.setColumns(10);
+		
+		DireccionCorreo = new JTextField();
+		DireccionCorreo.setBounds(184, 118, 201, 20);
+		getContentPane().add(DireccionCorreo);
+		DireccionCorreo.setColumns(10);
+		
+		Nick = new JTextField();
+		Nick.setColumns(10);
+		Nick.setBounds(184, 145, 201, 20);
+		getContentPane().add(Nick);
+		
+		Contrasena = new JTextField();
+		Contrasena.setColumns(10);
+		Contrasena.setBounds(184, 172, 201, 20);
+		getContentPane().add(Contrasena);
+		
+		try {
+			conexion= DriverManager.getConnection("jdbc:mysql://localhost:3306/registros", "root", "");
+		}catch(Exception e) {
+			e.getStackTrace();
+			JOptionPane.showInputDialog(null,"Error al conectar a la base de datos");
+		}
+		
 		
 		
 	}
-	
-	
 }
